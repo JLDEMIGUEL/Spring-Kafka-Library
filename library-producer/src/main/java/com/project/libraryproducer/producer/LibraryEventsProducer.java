a943d5968b9c70092f37820f865ad3a856c9f2af
jldemiguel <jldmp2001@gmail.com>
2024-03-28 11:15:45 +0100

    Refactored producer

--- library-producer/src/main/java/com/project/libraryproducer/producer/LibraryEventsProducer.java
+++ library-producer/src/main/java/com/project/libraryproducer/producer/LibraryEventsProducer.java
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.libraryproducer.domain.LibraryEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Slf4j
@@ -26,7 +30,8 @@ public class LibraryEventsProducer {
     public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
         Integer key = libraryEvent.libraryEventId();
         String value = objectMapper.writeValueAsString(libraryEvent);
-        CompletableFuture<SendResult<Integer, String>> completableFuture = kafkaTemplate.send(topic, key, value);
+        ProducerRecord<Integer, String> producerRecord = buildProducerRecord(key, value);
+        CompletableFuture<SendResult<Integer, String>> completableFuture = kafkaTemplate.send(producerRecord);
 
         completableFuture.whenComplete(((sendResult, throwable) -> {
             if (throwable != null) {
@@ -35,6 +40,11 @@ public class LibraryEventsProducer {
                 handleSuccess(key, value);
             }
         }));
+    }
+
+    private ProducerRecord<Integer, String> buildProducerRecord(Integer key, String value) {
+        List<Header> recordHeaders = List.of(new RecordHeader("event-source", "scanner".getBytes()));
+        return new ProducerRecord<>(topic, null, key, value, recordHeaders);
     }
 
     private void handleSuccess(Integer key, String value) {
